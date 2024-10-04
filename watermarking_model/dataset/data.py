5bce8cb0c21852d6571ae830ffe0cad0259eb4a0
Yizhu Wen <wenyizhuuk@126.com>
2024-10-03 23:22:30 -1000

    change the loading location

--- watermarking_model/dataset/data.py
+++ watermarking_model/dataset/data.py
@@ -144,7 +144,7 @@ class mel_dataset_test(Dataset):
 
     def __getitem__(self, idx):
         audio_name = self.wavs[idx]
-        print(self.dataset_path)
+        print(audio_name)
         wav, sr = torchaudio.load(os.path.join(self.dataset_path, audio_name))
         # wav = self.resample(wav)
         # wav = wav[:,:self.max_len]
