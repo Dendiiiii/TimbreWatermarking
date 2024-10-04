5dac85c8b1b9905251f7028c7a5b738ab032e613
Yizhu Wen <wenyizhuuk@126.com>
2024-10-04 00:19:29 -1000

    add reading audio file code

--- watermarking_model/embed_and_save.py
+++ watermarking_model/embed_and_save.py
@@ -271,7 +271,7 @@ def main(args, configs):
             #             logging.info("Shift Amount Exceeded!")
             # ###################################################
                 encoded = wav[0].unsqueeze(0)
-                decoded = decoder.test_forward(wav[0].unsqueeze(0))
+                decoded = decoder.test_forward(wav[0].unsqueeze(0).cuda())
                 # losses = loss.en_de_loss(wav_matrix, encoded, msg, decoded)
                 decoder_acc = (decoded >= 0).eq(msg >= 0).sum().float() / msg.numel()
                 zero_tensor = torch.zeros(wav_matrix.shape).to(device)
